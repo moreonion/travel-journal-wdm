4adbd0e1363e53705c4889e9329efb672e2cdfcc
phaer <phaer@phaer.org>
2014-09-04 15:51:59 +0200

    add js to toggle background info.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    var toggleBackgroundInfo = function toggleBackgroundInfo() {
+        var wrapper = $('.en_right_wrapper');
+        wrapper.toggleClass('background-info');
+
+        if (wrapper.find('#thex').length === 0) {
+            wrapper.prepend(
+                $('<a href="#close" id="thex" ' +
+                  'title="Close the background info">Close</a>'
+                 ).on('click', toggleBackgroundInfo)
+            );
+        }
+    }
+
+    var windowResized = function(mql) {
+        if (mql.matches) { // window is at least 900px wide;
+            $('a[title="Click here to read the background info"]')
+                .on('click', toggleBackgroundInfo);
+        } else { // window is less than 900px wide;
+            $('a[title="Click here to read the background info"]')
+                .off('click', toggleBackgroundInfo);
+        }
+    }
+
+    var mql = window.matchMedia("(min-width: 900px)");
+    windowResized(mql);
+    mql.addListener(windowResized);
+
+});
